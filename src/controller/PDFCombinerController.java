0f6b6337c449f3a033a083994cd684e90098d895
Shaan Khan <shaankhan101@gmail.com>
2020-11-19 23:39:30 +1100

    Tested dragover
    Tested dragover function

--- src/controller/PDFCombinerController.java
+++ src/controller/PDFCombinerController.java
@@ -4,17 +4,50 @@ import dev.shaankhan.javafx.Controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 import model.PDFCombiner;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class PDFCombinerController extends Controller<PDFCombiner>
+
+public class PDFCombinerController extends Controller<PDFCombiner> implements Initializable
 {
+    @FXML
+    private ImageView imageView;
     @FXML
     private void exit(ActionEvent event) {
         Platform.exit();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void handleDragOver(DragEvent event) {
+        if(event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+    }
+
     public final PDFCombiner getPDFCombiner() {
         return model;
+    }
+
+    @FXML
+    private void handleDrop(DragEvent event) throws FileNotFoundException {
+        List<File> files = event.getDragboard().getFiles();
+        Image img = new Image(new FileInputStream(files.get(0)));
+        imageView.setImage(img);
     }
 }
--- src/model/PDFCombiner.java
+++ src/model/PDFCombiner.java
@@ -1,12 +1,25 @@
 package model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.File;
+
 public class PDFCombiner
 {
     //private final Thingy thing;
+    private final ObservableList<File> fileObservableList;
 
     public PDFCombiner() {
-
+        this.fileObservableList = FXCollections.observableArrayList();
     }
 
+    public ObservableList<File> getFileObservableList()
+    {
+        return fileObservableList;
+    }
 
+    public void addFile(File file) {
+        this.fileObservableList.add(file);
+    }
 }
